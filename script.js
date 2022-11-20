0e3bd990edd0e2f0bd7b870d45fde8bfce7f84ec
Sasha <aleksadr@gmail.com>
2022-11-20 18:27:21 +0300

    2 comm

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let userName = 'Alex'
 
 console.log(userName);
+
+
+const arr = [2, 5, 6];
+
+const arr1 = [];
+
+for (let i = 0; i < arr.length; i++) {
+    arr1[i] = arr[i];
+}
+
+console.log(arr1);
