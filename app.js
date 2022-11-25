807fb1dd43cf8e07d17b02f2133092e7abba73ab
Sasha <aleksadr@gmail.com>
2022-11-25 20:35:33 +0300

    fix5

---
+++ app.js
@@ -0,0 +1,85 @@
+'use strict';
+// const burger = null;
+// const kartoshka = false;
+// const jucy = 1;
+
+// if (burger || kartoshka && jucy) {
+//     console.log('Классно');
+// } 
+// else {
+//     console.log('Плохо');
+// }
+
+
+// console.log((burger || kartoshka && jucy));
+
+
+// const burger = 3;
+// const kartoshka = 1;
+// const jucy = 0;
+
+// console.log(burger === 3 && jucy && kartoshka);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'asdasdasd');
+
+// if (burger === 3 && jucy === 1 && kartoshka) {
+//     console.log('Все поели');
+// } else {
+//     console.log('Уходим мужики');
+// }
+
+// // console.log((burger === 3 && kartoshka));
+
+
+// let mes = 'Привет'
+
+// if (2 + 1 === 3 && '1' == 1 || 10 > 5 && 10 === 1) {
+//     console.log(mes);
+// }
+
+// if (0) {
+//     console.log('No');
+// } else if (' ') {
+//     console.log('Yes');
+// }
+
+// let userCounter = 0;
+// let newUsers = userCounter++;
+// console.log(newUsers);
+
+// console.log(!false && 11 || 18 && !'');
+
+// let name = 0;
+// console.log(name ?? 'Noname');
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(`Число ${i}`);
+// }
+
+// First: for (let i = 0; i < 2; i++) {
+
+//     for (let size = 0; size < 3; size++) {
+//         if (size == 1) {
+//             break First;
+//         }
+//         console.log(size);
+//     }
+// }
+
+// function showName() {
+//     console.log('Вася');
+// }
+// setTimeout(showName, 0);
+// console.log('Коля');
+
+if (2 > 1) {
+    function showMessage() {
+        console.log("Сообщение");
+    }
+}
+showMessage();
+
